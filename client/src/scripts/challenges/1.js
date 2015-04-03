fb6a156d3433db181d4b387bfd51f32129de8532
Riley Shaw <rileyjshaw@gmail.com>
2015-04-03 10:36:08 -0400

    Update challenge 1 to make use of the new verify() function

--- client/src/scripts/challenges/1.js
+++ client/src/scripts/challenges/1.js
@@ -3,18 +3,19 @@ var initialText = `var array = ['Donde', 'Esta', 'La', 'Biblioteca'];
 var i = 0;
 while (i < array.length) {
   if (i % 2 === 0) {
-    console.log(array[i]);
+    // we're on an even index
   }
 
   i++;
 }
+
+verify();
 `;
 
 module.exports = {
   title: 'for( ) the love of loops.',
   description: 'Now that we\'ve covered the <code>while</code> loop, it\'s time to take a look at its twin sister: the <code>for</code> loop. Iterate through the array using a <code>for</code> loop, and log the content for all <em>even</em> indices.',
   initialText: initialText,
-  whitelist: ['ForStatement'],
   blacklist: ['WhileStatement'],
   nestedRules: {
     ForStatement: {
@@ -25,8 +26,12 @@ module.exports = {
   },
   customRules: [
     {
-      description: 'Program must have the word \'foobar\' in it.',
+      description: 'Program must have the word \'foobar\' in it',
       fn(code) { return code.indexOf('foobar') > -1; }
     }
-  ]
+  ],
+  output: {
+    description: 'Program must pass the string "winner!" into the verify function',
+    fn: (str) => str === 'winner!'
+  }
 };
